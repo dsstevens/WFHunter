5484f7b9e504f48f161c0f933909f0caf6f0f7c9
Deanna Stevens <deanna.sofias@gmail.com>
2024-01-10 22:49:54 -0500

    Add React Router w basic paths

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 
 function App() {
+  const isHomePage = location.pathname === '/'
+
+
   return (
+    <main className='App'>
+      <Header isHomePage={isHomePage}>
+        {isHomePage ? null : <HomeButton />}
+      </Header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </main>
     
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <div className="Home">
+      <h1>WFH<span style="font-style:italic">unter</span></h1>
+    </div>
+  )
+}

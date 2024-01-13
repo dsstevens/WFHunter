fb45ad4c28c5b1ee840fa0f32adcf7539caf27a6
Deanna Stevens <deanna.sofias@gmail.com>
2024-01-13 16:17:14 -0500

    Add Proptypes to jobsContainer component

--- src/Components/JobsContainer/JobsContainer.js
+++ src/Components/JobsContainer/JobsContainer.js
@@ -1,6 +1,8 @@
 import './JobsContainer.css'
 import { Link } from "react-router-dom"
 import moment from 'moment'
+import PropTypes from 'prop-types'
+
 
 const JobsContainer = ({ jobs }) => {
   console.log(jobs, "this is jobs in jobscontainer", jobs[0])
@@ -22,5 +24,26 @@ const JobsContainer = ({ jobs }) => {
 
   )
  }
+
+ JobsContainer.propTypes = {
+  jobs: PropTypes.arrayOf(PropTypes.shape({
+    annualSalaryMax: PropTypes.string.isRequired,
+    annualSalaryMin: PropTypes.string.isRequired,
+    companyLogo: PropTypes.string.isRequired,
+    companyName: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    jobDescription: PropTypes.string.isRequired,
+    jobExcerpt: PropTypes.string.isRequired,
+    jobGeo: PropTypes.string.isRequired,
+    jobIndustry: PropTypes.arrayOf(PropTypes.string).isRequired,
+    jobLevel: PropTypes.string.isRequired,
+    jobTitle: PropTypes.string.isRequired,
+    jobType: PropTypes.arrayOf(PropTypes.string).isRequired,
+    pubDate: PropTypes.string.isRequired,
+    salaryCurrency: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+  })).isRequired
+ };
+ 
 
  export default JobsContainer

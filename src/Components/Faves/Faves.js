59d2c308857baf3ef9a919c4fb220f635b1c7a86
Deanna Stevens <deanna.sofias@gmail.com>
2024-01-13 16:14:59 -0500

    Add Proptypes to faves component

--- src/Components/Faves/Faves.js
+++ src/Components/Faves/Faves.js
@@ -1,4 +1,5 @@
 import JobsContainer from "../JobsContainer/JobsContainer"
+import PropTypes from 'prop-types'
 
 const Faves = ({ jobs, favorites }) => {
   const favoriteJobs = jobs.filter((job) => favorites.includes(job.id))
@@ -10,5 +11,27 @@ const Faves = ({ jobs, favorites }) => {
     </div>
   )
 }
+
+Faves.propTypes = {
+  jobs: PropTypes.arrayOf(PropTypes.shape({
+    annualSalaryMax: PropTypes.string.isRequired,
+    annualSalaryMin: PropTypes.string.isRequired,
+    companyLogo: PropTypes.string.isRequired,
+    companyName: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    jobDescription: PropTypes.string.isRequired,
+    jobExcerpt: PropTypes.string.isRequired,
+    jobGeo: PropTypes.string.isRequired,
+    jobIndustry: PropTypes.arrayOf(PropTypes.string).isRequired,
+    jobLevel: PropTypes.string.isRequired,
+    jobTitle: PropTypes.string.isRequired,
+    jobType: PropTypes.arrayOf(PropTypes.string).isRequired,
+    pubDate: PropTypes.string.isRequired,
+    salaryCurrency: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+  })).isRequired,
+  favorites: PropTypes.arrayOf(PropTypes.number).isRequired,
+ };
+ 
 
 export default Faves
